f50528c9b896e3b1b8f62ce8fab928585873edff
Michael Poust <michael.poust221@gmail.com>
2022-06-04 18:35:56 -0400

    test return types and void methods

---
+++ TestNinja.UnitTests/CustomerControllerTests.cs
@@ -0,0 +1,46 @@
+﻿
+using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class CustomerControllerTests
+    {
+
+        private CustomerController _controller;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _controller = new CustomerController();
+        }
+
+        [Test]
+        public void GetCustomer_IdIsZero_ReturnNotFound()
+        {
+            // Act
+            var result = _controller.GetCustomer(0);
+
+            // Assert
+            // Explicitly a NotFound object
+            Assert.That(result, Is.TypeOf<NotFound>());
+
+            // NotFound or one of its derivatives
+            //Assert.That(result, Is.InstanceOf<NotFound>());
+        }
+
+        [Test]
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        public void GetCustomer_IdIsNotZero_ReturnOk(int custNum)
+        {
+            // Act
+            var result = _controller.GetCustomer(custNum);
+
+            // Assert
+            Assert.That(result, Is.TypeOf<Ok>());
+        }
+    }
+}
---
+++ TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class ErrorLoggerTests
+    {
+        private ErrorLogger _logger;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _logger = new ErrorLogger();
+        }
+
+        [Test]
+        [TestCase("Error Test Message")]
+        public void Log_WhenCalled_SetTheLastErrorProperty(string logMessage)
+        {
+            // Act
+            _logger.Log(logMessage);
+
+            // Assert
+            Assert.That(_logger.LastError, Is.EqualTo(logMessage));
+        }
+    }
+}
