80069a4033e4b078c09fbc6c23c4fbfc1edceb00
Michael Poust <michael.poust221@gmail.com>
2022-06-04 15:22:23 -0400

    convert Math to parameterized tests

--- TestNinja.UnitTests/MathTests.cs
+++ TestNinja.UnitTests/MathTests.cs
@@ -25,34 +25,16 @@ namespace TestNinja.UnitTests
         }
 
         [Test]
-        public void Max_FirstArgumentIsGreater_ReturnTheFirstArgument()
+        [TestCase(2, 1, 2)]
+        [TestCase(1, 2, 2)]
+        [TestCase(1, 1, 1)]
+        public void Max_WhenCalled_ReturnTheGreaterArgument(int a, int b, int expectedResult)
         {
             // Act
-            var result = _math.Max(2, 1);
+            var result = _math.Max(a, b);
 
             // Assert
-            Assert.That(result, Is.EqualTo(2));
-        }
-
-        [Test]
-        public void Max_SecondArgumentIsGreater_ReturnTheSecondArgument()
-        {
-            // Act
-            var result = _math.Max(1, 2);
-
-            // Assert
-            Assert.That(result, Is.EqualTo(2));
-
-        }
-
-        [Test]
-        public void Max_ArgumentsAreEqual_ReturnTheSameArgument()
-        {
-            // Act
-            var result = _math.Max(2, 2);
-
-            // Assert
-            Assert.That(result, Is.EqualTo(2));
+            Assert.That(result, Is.EqualTo(expectedResult));
         }
     }
 }
