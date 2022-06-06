173e45634c913e3edafaaef617f1fed5f153299f
Michael Poust <mpoust@availtec.com>
2022-06-06 09:12:45 -0400

    use moq object for videoservice tests with filereader

--- TestNinja.UnitTests/VideoServiceTests.cs
+++ TestNinja.UnitTests/VideoServiceTests.cs
@@ -1,16 +1,28 @@
-﻿using NUnit.Framework;
+﻿using Moq;
+using NUnit.Framework;
 using TestNinja.Mocking;
-using TestNinja.UnitTests.Mocks;
 
 namespace TestNinja.UnitTests
 {
     [TestFixture]
     public class VideoServiceTests
     {
+        private VideoService _videoService;
+        private Mock<IFileReader> _mockFileReader;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockFileReader = new Mock<IFileReader>();
+            _videoService = new VideoService(_mockFileReader.Object);
+        }
+
         [Test]
         public void ReadVideoTitle_EmptyFile_ReturnError()
         {
-            var service = new VideoService(new MockFileReader());
+            _mockFileReader.Setup(fr => fr.Read("video.txt")).Returns("");
+
+            var service = new VideoService(_mockFileReader.Object);
 
             var result = service.ReadVideoTitle();
 
