2bea9555fa18d6307874d7a1ac6cb9c3a3faa7e7
Michael Poust <mpoust@availtec.com>
2022-06-06 12:24:15 -0400

    employee controller tests

---
+++ TestNinja.UnitTests/EmployeeControllerTests.cs
@@ -0,0 +1,38 @@
+﻿
+using Moq;
+using NUnit.Framework;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class EmployeeControllerTests
+    {
+        private EmployeeController _controller;
+        private Mock<IEmployeeService> _mockEmployeeService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockEmployeeService = new Mock<IEmployeeService>();
+            _controller = new EmployeeController(_mockEmployeeService.Object);
+        }
+
+        [Test]
+        [TestCase(1)]
+        public void DeleteEmployee_WhenCalled_DeleteTheEmployeeFromDb(int id)
+        {
+            _controller.DeleteEmployee(id);
+
+            _mockEmployeeService.Verify(s => s.DeleteEmployee(id));
+        }
+
+        [Test]
+        public void DeleteEmployee_WhenCalled_ReturnsRedirectResult()
+        {
+            var result = _controller.DeleteEmployee(1);
+
+            Assert.That(result, Is.TypeOf<RedirectResult>());
+        }
+    }
+}
